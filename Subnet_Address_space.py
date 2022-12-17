2082542af80f6cfcc00b36df5df2bcc7ca83d171
NigeleM <mccoy.nigele@gmail.com>
2022-12-16 22:01:05 -0600

    Added more details

--- Subnet_Address_space.py
+++ Subnet_Address_space.py
@@ -1,12 +1,18 @@
 
+import ipaddress
 # Code for address 
+startIp = int("Enter starting IP: ")
+endIp = int("Enter ending IP: ")
 stIp = ipaddress.IPv6Address(startIp)
 edIp = ipaddress.IPv6Address(endIp)
 ip = [ipaddr for ipaddr in ipaddress.summarize_address_range(stIp, edIp)]
 
 # Code for IP generator
-start_ip = ipaddress.IPv4Address(ipcontent[2])
-end_ip = ipaddress.IPv4Address(ipcontent[3])
+startIp = int("Enter starting IP: ")
+endIp = int("Enter ending IP: ")
+# can also put logic in a loop for files
+start_ip = ipaddress.IPv4Address(startIp)
+end_ip = ipaddress.IPv4Address(endIp)
 for ip_int in range(int(start_ip), int(end_ip)):
       print(ipaddress.IPv4Address(ip_int))
       
